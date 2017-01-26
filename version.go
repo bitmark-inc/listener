6982e2350b365b02c3d05780aa591c0eec9a1949
Christopher Hall <hsw@bitmark.com>
2017-01-26 15:03:25 +0800

    fix package conflict, update copyright
    Signed-off-by: Christopher Hall <hsw@bitmark.com>

--- version.go
+++ version.go
@@ -1,11 +1,11 @@
-// Copyright (c) 2014-2016 Bitmark Inc.
+// Copyright (c) 2014-2017 Bitmark Inc.
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
-package logger
+package listener
 
 const (
 	Major   = "1"
-	Minor   = "1"
+	Minor   = "2"
 	Version = Major + "." + Minor
 )
